1cbb9cbabab8194a681d7a5af6f4015045c893c0
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-07-17 05:21:23 +0530

    Add: website is live ! 🎉

---
+++ precache-manifest.ef269d62dfb3afdc8f1b66463c6e4124.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c09ad6a3720968bd80f91e4b5f32065f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d0e6e5375dd934e852",
+    "url": "/static/css/main.21e3536b.chunk.css"
+  },
+  {
+    "revision": "40dae1ed45dc8f87ae58",
+    "url": "/static/js/2.9d811659.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9d811659.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d0e6e5375dd934e852",
+    "url": "/static/js/main.b403b808.chunk.js"
+  },
+  {
+    "revision": "c888520825ff7179b91f",
+    "url": "/static/js/runtime-main.c35f0221.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "428642ddd08c4df6f52739e0bc6bba11",
+    "url": "/static/media/header-bg.428642dd.png"
+  },
+  {
+    "revision": "08bd398399c6d8f11eb59fa10b2a7e04",
+    "url": "/static/media/logo.08bd3983.png"
+  }
+]);
