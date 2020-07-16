8cf2913c74e8162b14f8cb8786187b2a06431537
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-07-17 01:55:02 +0530

    Add: Nav and SCSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
+import "./styles/main.scss";
+import Nav from "./components/Nav-component";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Programming Zone</h1>
-      </header>
+      <Nav />
     </div>
   );
 }
---
+++ src/components/Nav-component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import logo from "../logo.png";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <div className="navigation-group">
+          <h1>Programming Zone on Github</h1>
+        </div>
+        <div>
+          <img width="64px" height="64px" src={logo} alt="logo" />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
