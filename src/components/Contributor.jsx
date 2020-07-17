c41862097e2d82a0afcbd9f6c8d3dcbd484da558
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-07-18 01:10:19 +0530

    Add: contributors 👋

--- src/components/Contributor.jsx
+++ src/components/Contributor.jsx
@@ -1,9 +1,44 @@
 import React from "react";
+import Profile from "./Profile";
+import userData from "./userData";
 
 const Contributor = () => {
+  const defaultImg =
+    "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png";
+  const listProfile = userData.map((user, id) => (
+    <Profile
+      key={id}
+      name={user.name}
+      img={user.img === null ? defaultImg : user.img}
+      username={user.username}
+      userlink={user.userlink}
+      isOrg={user.isOrg}
+      orgName={user.orgName}
+      orgLink={user.orgLink}
+      isLoc={user.isLoc}
+      Loc={user.Loc}
+    />
+  ));
   return (
     <div className="contributor-div">
-      <h2>Contributors</h2>
+      <div>
+        <h2>Main Contributors</h2>
+        <div className="con-container">{listProfile}</div>
+      </div>
+      <div>
+        <h2>Want to contribute?</h2>
+        <p>
+          We know groups can accomplish more than individuals. That’s why our
+          mission is to be the catalyst in communities of contributors, and
+          partners creating better technology the open source way.
+          <br />
+          <br />
+          <a className="blue-link" href="https://github.com/programming-zone">
+            Let's Contribute{" "}
+          </a>
+          <i className=" blue-link fa fa-arrow-right "></i>
+        </p>
+      </div>
     </div>
   );
 };
---
+++ src/components/userData.js
@@ -0,0 +1,29 @@
+const userData = [
+  {
+    id: 1,
+    name: "Injamul Mohammad Mollah",
+    img:
+      "https://avatars1.githubusercontent.com/u/43756219?s=460&u=c133586bd27db098039b668856fb7c3f1cbbea04&v=4",
+    username: "mrinjamul",
+    userlink: "https://github.com/mrinjamul",
+    isOrg: false,
+    orgName: "Github",
+    orgLink: "https://github.com",
+    isLoc: true,
+    Loc: "Berhampore, IN",
+  },
+  {
+    id: 2,
+    name: "Injamul",
+    img: null,
+    username: "linuxuserin",
+    userlink: "https://github.com/linuxuserin",
+    isOrg: true,
+    orgName: "Programming Zone",
+    orgLink: "https://programming-zone.github.io/",
+    isLoc: true,
+    Loc: "Earth",
+  },
+];
+
+export default userData;
